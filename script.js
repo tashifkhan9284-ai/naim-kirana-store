da7a3fa69d01a757b665937f6e2a1459115b69e7
tashifkhan9284-ai <tashifkhan9284@gmail.com>
2026-07-05 18:38:20 +0530

    Update script.js

--- script.js
+++ script.js
@@ -12,7 +12,6 @@ fetch("products.json")
       card.innerHTML = `
         <img src="${product.image}" alt="${product.name}">
         <h3>${product.name}</h3>
-        <p>Price: ₹${product.price}</p>
         <p>Category: ${product.category}</p>
       `;
 
